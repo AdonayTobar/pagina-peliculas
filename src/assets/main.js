7c1e9f9f6f853cac911804bf1e76e1c942d5c24d
Adonay Tobar <tobaradonay@gmail.com>
2023-03-17 13:08:28 -0600

    detalles para mejorar

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,5 +1,6 @@
 
-const slider = document.querySelector('.carrusel');
+function carImg(){
+  const slider = document.querySelector('.carrusel');
 let sliderSeccion = document.querySelectorAll('.slider');
 let sliderSeccionUltimo = sliderSeccion[sliderSeccion.length -1];
 
@@ -39,36 +40,524 @@ setInterval(function() {
     derecha();
 },5000);
 
+
+}
+
 //El boton
-const searchBox = document.querySelector(".search-box");
+
+//busqueda
+
+function busqueda(){
+  const searchBox = document.querySelector(".search-box");
       const searchBtn = document.querySelector(".search-icon");
       const cancelBtn = document.querySelector(".cancel-icon");
       const searchInput = document.querySelector("input");
       const searchData = document.querySelector(".search-data");
-      searchBtn.onclick =()=>{
-        searchBox.classList.add("active");
-        searchBtn.classList.add("active");
-        searchInput.classList.add("active");
-        cancelBtn.classList.add("active");
-        searchInput.focus();
-        if(searchInput.value != ""){
+
+      searchBtn.onclick = () => {
+        if (!searchBox.classList.contains("active")) { // Si la caja de búsqueda no está activa
+          searchBox.classList.add("active");
+          searchBtn.classList.add("active");
+          searchInput.classList.add("active");
+          cancelBtn.classList.add("active");
+          searchData.textContent = "";
+          searchInput.focus();
+          if(searchInput.value != ""){
           var values = searchInput.value;
           searchData.classList.remove("active");
-          searchData.innerHTML = "You just typed " + "<span style='font-weight: 500;'>" + values + "</span>";
+          searchData.innerHTML = "Resultados de: " + "<span style='font-weight: 500;'>" + values + "</span>";
         }else{
           searchData.textContent = "";
+        } 
+        } else { // Si la caja de búsqueda está activa
+          if(!searchInput.value){
+            searchData.classList.remove("active");
+            searchData.innerHTML = "Escribe el nombre " + "<span style='font-weight: 500;'>" + "</span>";
+          } else {
+            location.hash = '#search=' + searchInput.value;
+            searchData.classList.add("active");
+          }
+          
         }
-      }
-      cancelBtn.onclick =()=>{
+      };
+      
+
+      function cerrar(){
         searchBox.classList.remove("active");
         searchBtn.classList.remove("active");
         searchInput.classList.remove("active");
         cancelBtn.classList.remove("active");
-        searchData.classList.toggle("active");
+        searchData.classList.toggle("inactive");
         searchInput.value = "";
       }
+      cancelBtn.onclick = cerrar;
+      cerrar();
+}
 
 
+
+
+
+//Hoy inicia lo chido 
+//Codigo dedicado a todo lo de JS
+
+const api = axios.create({
+  baseURL: 'https://api.themoviedb.org/3/',
+  Headers: {
+    'Content-Type': 'application/json;charset=utf-8',
+  },
+  params: {
+    'api_key': API_KEY,
+    'language': 'es-MX'
+  }
+});
+
+async function getTrendingMoviesPreview(){
+
+
+  const {data} = await api('trending/movie/day');
+
+
+  const movies = data.results;
+  console.log({data, movies});
+
+  
+
+
+  
+
+  movies.forEach(movie => {
+
+    // Crear el elemento principal 'div' con la clase 'slider'
+const sliderDiv = document.createElement('div');
+sliderDiv.classList.add('slider');
+
+// Crear el elemento 'div' con la clase 'im' y su hijo 'img'
+const imDiv = document.createElement('div');
+imDiv.classList.add('im');
+const imgC = document.createElement('img');
+imgC.src = 'https://image.tmdb.org/t/p/w500' + movie.poster_path;
+imgC.alt = '';
+imDiv.appendChild(imgC);
+
+// Crear el elemento 'div' con la clase 'tex' y sus hijos 'h2', 'p' y 'button'
+const texDiv = document.createElement('div');
+texDiv.classList.add('tex');
+const title1 = document.createElement('h2');
+title1.textContent = movie.title;
+//const p = document.createElement('p');
+//p.textContent = movie.overview;
+const button = document.createElement('button');
+button.textContent = 'Detalles';
+texDiv.appendChild(title1);
+//texDiv.appendChild(p);
+texDiv.appendChild(button);
+//Haciendo que el botkn me lleve a detalles
+button.addEventListener("click", () => {
+  location.hash = '#movie=' + movie.id;
+})
+
+// Agregar los elementos hijos al elemento padre 'div.slider'
+sliderDiv.appendChild(imDiv);
+sliderDiv.appendChild(texDiv);
+
+// Agregar el elemento 'div.slider' al documento HTML
+contenedorCarrusel.appendChild(sliderDiv);
+
+
+
+
+
+    // Crear el elemento article
+const article = document.createElement('article');
+article.classList.add('carrusell');
+article.addEventListener("click", () => {
+  location.hash = '#movie=' + movie.id;
+})
+
+// Crear el elemento img y añadirlo al article
+const imgM = document.createElement('img');
+imgM.src = 'https://image.tmdb.org/t/p/w300' + movie.poster_path;
+imgM.alt = movie.title;
+article.appendChild(imgM);
+
+// Crear el elemento div y añadirlo al article
+const textDiv = document.createElement('div');
+textDiv.classList.add('text');
+const title = document.createElement('h2');
+title.textContent = movie.title;
+textDiv.appendChild(title);
+article.appendChild(textDiv);
+
+
+//Anadiendo el articulo al contendor principal
+contenedorMovies.appendChild(article);
+    
+  });
+}
+
+async function getCategoriesPreview(){
+
+  const {data} = await api('genre/movie/list');
+ 
+
+  const categories = data.genres;
+  console.log({data, categories});
+
+  //Creando la vista del contenedor principal de peliculas del carrusel
+let contenedorCategories = document.querySelector('.cate .contenedorCate');
+
+  categories.forEach(category => {
+
+
+// Creamos un nuevo elemento article
+const articleElement = document.createElement('article');
+
+// Creamos un nuevo elemento div y lo añadimos como hijo del article
+const divElement = document.createElement('div');
+articleElement.appendChild(divElement);
+
+// Creamos un nuevo elemento p y le añadimos el texto "Accion"
+const pElement = document.createElement('p');
+pElement.textContent = category.name;
+pElement.setAttribute('id', 'id'+ category.id);
+pElement.addEventListener('click', () => {
+  location.hash = '#category=' + category.id + '-' + category.name;
+})
+articleElement.appendChild(pElement);
+
+
+
+// Añadimos el article al DOM
+contenedorCategories.appendChild(articleElement);
+    
+  });
+}
+
+
+async function getMoviesByCategory(id){
+  window.scroll(0,0);
+  const {data} = await api('discover/movie', {
+    params: {
+      with_genres: id,
+    }
+  });
+
+
+  const movies = data.results;
+  console.log({data, movies});
+
+  const peliculasCate = document.querySelector(".catePeliculas .peliculas1M");
+
+  peliculasCate.innerHTML = '';
+  movies.forEach(movie => {
+
+    //nombreCate.textContent = 
+    
+    // Crear el elemento article
+  const article = document.createElement("article");
+  article.classList.add("carrusellM");
+
+  // Crear el elemento div para la imagen
+  const imgDiv = document.createElement("div");
+  imgDiv.classList.add("img");
+  article.appendChild(imgDiv);
+
+  article.addEventListener("click", () => {
+    location.hash = '#movie=' + movie.id;
+  });
+
+  // Crear el elemento img y establecer su atributo src
+  const imgCateg = document.createElement("img");
+  imgCateg.src = 'https://image.tmdb.org/t/p/w300' + movie.poster_path;
+  imgDiv.appendChild(imgCateg);
+
+  // Crear el elemento div para el texto
+  const textDiv = document.createElement("div");
+  textDiv.classList.add("text");
+
+
+  // Crear el elemento h2 y establecer su contenido de texto
+  const h2 = document.createElement("h2");
+  h2.textContent = movie.title;
+  textDiv.appendChild(h2);
+
+  article.appendChild(textDiv);
+  // Agregar el artículo al documento
+  peliculasCate.appendChild(article);
+
+    
+  });
+}
+
+async function getMoviesBySearch(query){
+  window.scroll(0,0);
+  const {data} = await api('search/movie', {
+    params: {
+      query,
+    }
+  });
+
+
+  const movies = data.results;
+  console.log('Estas son las busquedas');
+  console.log({data, movies});
+
+  
+
+  let peliculasBusqueda = document.querySelector(".masPeliculas .busquedapelis");
+  
+  peliculasBusqueda.innerHTML = '';
+  
+  if(!movies.length){
+  resultado.textContent = 'No existen Peliculas con ese nombre';
+  resultado.style.color = "white";
+  resultado.style.fontSize = "3rem";
+  resultado.style.paddingBottom = "5rem";
+  } else {
+  resultado.textContent = 'Resultados de: ' + query;
+  resultado.style.color = "white";
+  resultado.style.fontSize = "2rem";
+  resultado.style.paddingBottom = "5rem";
+  }
+  movies.forEach(movie => {
+
+    //nombreCate.textContent = 
+    
+    // Crear el elemento article
+  const article1 = document.createElement("article");
+  article1.classList.add("carrusellM");
+
+  article1.addEventListener("click", () => {
+    location.hash = '#movie=' + movie.id;
+  });
+
+  // Crear el elemento div para la imagen
+  const imgDiv1 = document.createElement("div");
+  imgDiv1.classList.add("img");
+  article1.appendChild(imgDiv1);
+
+  // Crear el elemento img y establecer su atributo src
+  const imgB = document.createElement("img");
+  imgB.src = 'https://image.tmdb.org/t/p/w300' + movie.poster_path;
+  imgDiv1.appendChild(imgB);
+
+  // Crear el elemento div para el texto
+  const textDiv1 = document.createElement("div");
+  textDiv1.classList.add("text");
+
+
+  // Crear el elemento h2 y establecer su contenido de texto
+  const h21 = document.createElement("h2");
+  h21.textContent = movie.title;
+  textDiv1.appendChild(h21);
+
+  article1.appendChild(textDiv1);
+  // Agregar el artículo al documento
+  peliculasBusqueda.appendChild(article1);
+
+    
+  });
+  busqueda();
+}
+
+async function getTrendingMovies(){
+  window.scroll(0,0);
+  const {data} = await api('trending/movie/day');
+
+
+  const movies = data.results;
+  console.log('Estas son las nuevas peliculas');
+  console.log({data, movies});
+
+  
+
+  let peliculasBusqueda = document.querySelector(".masPeliculas .busquedapelis");
+  
+  peliculasBusqueda.innerHTML = '';
+  resultado.textContent = 'Tendencias';
+  resultado.style.color = "white";
+  resultado.style.fontSize = "5rem";
+  resultado.style.paddingBottom = "5rem";
+  movies.forEach(movie => {
+
+    //nombreCate.textContent = 
+    
+    // Crear el elemento article
+  const article1 = document.createElement("article");
+  article1.classList.add("carrusellM");
+
+  article1.addEventListener("click", () => {
+    location.hash = '#movie=' + movie.id;
+  });
+
+  // Crear el elemento div para la imagen
+  const imgDiv1 = document.createElement("div");
+  imgDiv1.classList.add("img");
+  article1.appendChild(imgDiv1);
+
+  // Crear el elemento img y establecer su atributo src
+  const imgB = document.createElement("img");
+  imgB.src = 'https://image.tmdb.org/t/p/w300' + movie.poster_path;
+  imgDiv1.appendChild(imgB);
+
+  // Crear el elemento div para el texto
+  const textDiv1 = document.createElement("div");
+  textDiv1.classList.add("text");
+
+
+  // Crear el elemento h2 y establecer su contenido de texto
+  const h21 = document.createElement("h2");
+  h21.textContent = movie.title;
+  textDiv1.appendChild(h21);
+
+  article1.appendChild(textDiv1);
+  // Agregar el artículo al documento
+  peliculasBusqueda.appendChild(article1);
+
+    
+  });
+  busqueda();
+}
+
+
+async function getMovieById(id){
+  //movie porque solo recibo una pelicula
+  const {data: movie} = await api('movie/' + id);
+
+  const container = document.querySelector('.detalles .carruselD');
+  container.innerHTML ="";
+
+
+
+console.log(movie);
+
+// Crear el contenedor principal de detalles de peliculas
+
+
+const sliderD = document.createElement('div');
+sliderD.classList.add('sliderD');
+
+// Crear el contenedor de la imagen
+const imD = document.createElement('div');
+imD.classList.add('imD');
+const imagen = document.createElement('img');
+imagen.src = 'https://image.tmdb.org/t/p/w500' + movie.poster_path;
+imagen.alt = movie.title;
+imD.appendChild(imagen);
+
+
+// Crear el contenedor del texto
+const tex = document.createElement('div');
+tex.classList.add('tex');
+
+const titulo = document.createElement('h2');
+titulo.innerText = movie.title;
+
+const cal = document.createElement('div');
+cal.classList.add('cal');
+
+const icono = document.createElement('img');
+icono.src = './assets/icon/star-svgrepo-com.svg';
+icono.alt = '';
+
+const puntuacion = document.createElement('p');
+puntuacion.innerText = " " + movie.vote_average.toFixed(1);
+cal.appendChild(icono);
+cal.appendChild(puntuacion);
+const descripcion = document.createElement('p');
+descripcion.innerText = movie.overview;
+tex.appendChild(titulo);
+tex.appendChild(cal);
+tex.appendChild(descripcion);
+
+
+// Agregar los contenedores al contenedor principal
+sliderD.appendChild(imD);
+sliderD.appendChild(tex);
+
+// Agregar el contenedor principal al contenedor existente en el HTML
+container.appendChild(sliderD);
+
+
+
+
+//Creando el contenedor de categorias de peliculas
+
+let catesById = movie.genres;
+console.log(catesById);
+
+const contenedorCateDetalles = document.querySelector('.detalles .columna')
+contenedorCateDetalles.innerHTML = "";
+
+catesById.forEach(cate => {
+// Creamos el elemento HTML
+const article = document.createElement("article");
+const articleDiv = document.createElement("div");
+const articleTitulo = document.createElement("p");
+
+// Agregamos las clases y textos necesarios
+articleTitulo.textContent = cate.name;
+articleTitulo.setAttribute('id', 'id'+ cate.id);
+articleTitulo.addEventListener('click', () => {
+  location.hash = '#category=' + cate.id + '-' + cate.name;
+})
+// Agregamos los elementos al documento
+article.appendChild(articleDiv);
+article.appendChild(articleTitulo);
+
+
+
+contenedorCateDetalles.appendChild(article);
+})
+
+
+
+
+getMoviesSimilares(id);
+detallePelicula.scrollTo(0,0);
+
+}
+
+async function getMoviesSimilares(id){
+  const {data} = await api('movie/' + id + '/similar');
+  const movies = data.results;
+  console.log('Estas son las similares');
+  console.log(data);
+
+
+  const moviesimilar = document.querySelector('.detalles .peliculas1');
+
+  moviesimilar.innerHTML = "";
+  movies.forEach(movie => {
+        // Crear el elemento article
+const article = document.createElement('article');
+article.classList.add('carrusell');
+article.addEventListener("click", () => {
+  location.hash = '#movie=' + movie.id;
+})
+
+// Crear el elemento img y añadirlo al article
+const imgM = document.createElement('img');
+imgM.src = 'https://image.tmdb.org/t/p/w300' + movie.poster_path;
+imgM.alt = movie.title;
+article.appendChild(imgM);
+
+// Crear el elemento div y añadirlo al article
+const textDiv = document.createElement('div');
+textDiv.classList.add('text');
+const title = document.createElement('h2');
+title.textContent = movie.title;
+textDiv.appendChild(title);
+article.appendChild(textDiv);
+
+
+//Anadiendo el articulo al contendor principal
+moviesimilar.appendChild(article);
+
+  });
+
+}
 
 
 
---
+++ src/assets/navegacion.js
@@ -0,0 +1,137 @@
+window.addEventListener('DOMContentLoaded', navegador,false);
+window.addEventListener('hashchange', navegador,false);
+
+//Haciendo que el boton de home, me lleve a home
+home.addEventListener('click', () => {
+    location.hash = '#home';
+})
+
+//Haciendo que el boton de ver mas me lleve a trends
+btnVerMas.addEventListener("click", () => {
+    location.hash = '#trends';
+});
+
+//Haciendo que la flecha de regresar me lleve siempre al home
+flechaRegresar.addEventListener("click", () => {
+    location.hash = window.history.back();//Esto sirve para regresar a la pagina anterior
+})
+
+function navegador(){
+    console.log({location});
+
+    if(location.hash.startsWith('#trends')) {
+        trendsPage();
+    } else if(location.hash.startsWith('#search=')) {
+        searchPage();
+    } else if(location.hash.startsWith('#movie=')) {
+        moviePage();
+    } else if(location.hash.startsWith('#category=')) {
+        categoriesPage();
+    }  else if(location.hash.startsWith('#home')) {
+        homePage();
+    } else {
+        homePage();
+    }
+}
+
+function homePage(){
+
+    flechaRegresar.classList.add('inactive');
+    detallePelicula.classList.add('inactive');
+    masPeliculas.classList.add('inactive');
+    pelisPorCatego.classList.add('inactive');
+
+    //Por si los elementos que vamos a mostrar tienen inactive
+    carruselImagenes.classList.remove('inactive');
+    contendorPelisyCate.classList.remove('inactive');
+    barraBusqueda.classList.remove('inactive');
+
+    console.log('Home');
+    busqueda();
+
+    const childrenCategoriesPreview = Array.from(contenedorMovies.children);
+  if(!childrenCategoriesPreview.length){
+    getTrendingMoviesPreview();
+    getCategoriesPreview();
+    carImg();
+  }
+
+    
+    
+}
+function searchPage(){
+    console.log('#search=');
+    detallePelicula.classList.add('inactive');
+    masPeliculas.classList.remove('inactive');
+    carruselImagenes.classList.add('inactive');
+    contendorPelisyCate.classList.add('inactive');
+    //Por si los elementos que vamos a mostrar tienen inactive
+    pelisPorCatego.classList.add('inactive');
+    barraBusqueda.classList.remove('inactive');
+    flechaRegresar.classList.remove('inactive');
+    resultado.classList.remove('inactive');
+
+    const [_, query] = location.hash.split('=');
+    if(!query){
+        location.hash = '#home';
+    }else {
+        getMoviesBySearch(query);
+    }
+    
+}
+function moviePage(){
+    console.log('#movie=');
+    detallePelicula.classList.remove('inactive');
+    flechaRegresar.classList.remove('inactive');
+
+    masPeliculas.classList.add('inactive');
+    pelisPorCatego.classList.add('inactive');
+    carruselImagenes.classList.add('inactive');
+    contendorPelisyCate.classList.add('inactive');
+    barraBusqueda.classList.add('inactive');
+
+    const [_, movieId] = location.hash.split('=');
+
+    getMovieById(movieId);
+
+}
+function categoriesPage(){
+    
+    detallePelicula.classList.add('inactive');
+    masPeliculas.classList.add('inactive');
+    carruselImagenes.classList.add('inactive');
+    contendorPelisyCate.classList.add('inactive');
+    //Por si los elementos que vamos a mostrar tienen inactive
+    pelisPorCatego.classList.remove('inactive');
+    barraBusqueda.classList.remove('inactive');
+    flechaRegresar.classList.remove('inactive');
+
+    console.log('#category=');
+
+    const [_, categoryInfo] = location.hash.split('=');//category, id-name, eso se hace con manipulacion de array
+    const [categoryID, categoryName] = categoryInfo.split('-');
+    const nombreBien= decodeURI(categoryName);
+
+    nombreCate.innerHTML = nombreBien;
+    getMoviesByCategory(categoryID);
+}
+function trendsPage(){
+    console.log('Trends!');
+
+    flechaRegresar.classList.remove('inactive');
+    detallePelicula.classList.add('inactive');
+    masPeliculas.classList.remove('inactive');
+    pelisPorCatego.classList.add('inactive');
+
+    //Por si los elementos que vamos a mostrar tienen inactive
+    carruselImagenes.classList.add('inactive');
+    contendorPelisyCate.classList.add('inactive');
+    barraBusqueda.classList.add('inactive');
+    resultado.classList.remove('inactive');
+  
+    window.scroll(0,0);//Esto se utiliza para que la pagina que se muestre siempre se muestre desde arriba
+    getTrendingMovies();
+}
+
+
+//location.hash = '#category='
